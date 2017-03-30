db22b169b1d021eb77db1ff7a9255b2f9dfaa7cc
zhangyongkang <zhangyongkang@tenxcloud.com>
2017-03-30 19:57:56 +0800

    correct middleware

--- skeleton/context/middleware.go
+++ skeleton/context/middleware.go
@@ -2,6 +2,11 @@ package context
 
 type Middleware func(ctx *Context)
 
+/*
+wrap should like this:
+wrap(ProcessRequest, ...Middleware)ProcessRequest
+*/
+
 func WrapMiddleWare(ctx *Context, middles ...Middleware) {
 	if ctx.Resp.ReplyFunc != nil {
 		ctx.Reply()
